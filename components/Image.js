f4fa6ee7a7527385c7168b41c9e83911be625a05
Patrick Stoica <hipatark@gmail.com>
2017-06-15 02:51:44 -0400

    disable auto since safari is requesting webp

--- components/Image.js
+++ components/Image.js
@@ -3,6 +3,7 @@ import Imgix from "react-imgix";
 export default ({ src, ...props }) => {
   return (
     <Imgix
+      auto={[]}
       aggressiveLoad={true}
       customParams={{ format: "jpg" }}
       src={`https://www.datocms-assets.com${src}`}
