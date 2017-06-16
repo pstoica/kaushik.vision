34f02dbe7ad6671ed8d7abb2cfc9714540b87ef7
Patrick Stoica <hipatark@gmail.com>
2017-06-15 20:47:04 -0400

    responsive type for gallery hover

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,9 @@ const Title = styled.h3`
   height: 100%;
 
   text-transform: lowercase;
-  font-size: ${p => p.theme.modularScale(3)};
   font-weight: regular;
+  font-size: ${p => p.theme.modularScale(1)};
+  ${p => p.theme.media.sm`font-size: ${p.theme.modularScale(3)};`}
 
   background-color: ${p => opacify(-0.3, p.theme.colors.blue)};
   color: ${p => p.theme.colors.white};
